352bafa305412d7b0ec920c91f94711aa7a7e93e
Francesco <dttfrancesco@gmail.com>
2023-05-05 23:11:06 +0200

    Cambiata dimensione delle descrizioni in tables.sql

--- Deliverables/DB Design/tables.sql
+++ Deliverables/DB Design/tables.sql
@@ -4,7 +4,7 @@ create table People(
 	Surname varchar(50) not null,
 	Age int unsigned not null check(Age >= 18),
 	Picture blob,
-	CV varchar(1023) not null,
+	CV varchar(1500) not null,
 	Role varchar(50) not null,
 
 	primary key(PersonID)
@@ -12,9 +12,9 @@ create table People(
 
 create table Projects(
 	ProjectID int unsigned auto_increment,
-	Title varchar(50) not null unique,
+	Title varchar(60) not null unique,
 	Preview varchar(100) not null,
-	Description varchar(1023) not null,
+	Description varchar(1500) not null,
 	Budget decimal(10,2) not null check(Budget >= 0),
 	Stage varchar(50) not null,
 	YearOfFoundation int unsigned not null check(YearOfFoundation >= 1000),
