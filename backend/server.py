0994f92fa6a6ed6bed67d7c9ccf70e4dba3d9234
Benedart <49732385+Benedart@users.noreply.github.com>
2023-05-06 16:32:41 +0200

    Changed library for mysql connection
    Hopefully this fixes concurrency problems
    Fixed get_project query
    Added utility queries

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-import mysql.connector
-from mysql.connector import Error
+from flask_mysqldb import MySQL
 import pandas as pd
 from dotenv import load_dotenv
 import json
@@ -17,17 +16,18 @@ db_user = os.getenv("user")
 db_password = os.getenv("password")
 db_database = os.getenv("database")
 
-# db connection handling
-db = mysql.connector.connect(
-  host=db_host,
-  user=db_user,
-  password=db_password,
-  database=db_database
-)
-
 # flask app
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
+app.config['MYSQL_HOST'] = db_host
+app.config['MYSQL_USER'] = db_user
+app.config['MYSQL_PASSWORD'] = db_password
+app.config['MYSQL_DB'] = db_database
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+ 
+mysql = MySQL(app)
+
 
 @app.route("/")
 def hello_world():
@@ -38,27 +38,29 @@ def hello_world():
 
 @app.route("/getProjects", methods=['GET'])
 def get_projects():
-    cursor = db.cursor(dictionary=True)
+    cursor = mysql.connection.cursor()
 
     cursor.execute("select projectid, title, preview, stage from projects")
     projects = cursor.fetchall()
+    cursor.close()
 
     return json.dumps(projects)
 
 
 @app.route("/getProject/<projectid>", methods=['GET'])
 def get_project(projectid):
-    cursor = db.cursor(dictionary=True)
+    cursor = mysql.connection.cursor()
 
     query = """
-        select projectid, p.title, p.description, budget, yearoffoundation, supervisor, name, surname, a.areaid, a.title as area, a.type
-        from ((projects as p join people on personId = supervisor) natural join refers as r) join areas as a on r.areaId = a.areaId
+        select projectid, p.title, p.description, stage, budget, yearoffoundation, supervisor, name, surname
+        from projects as p join people on personId = supervisor
         where ProjectID = %s
     """
     tuple = (projectid,)
 
     cursor.execute(query, tuple)
     project = cursor.fetchone()
+    cursor.close()
 
     # make the budget serializable for json
     # probably casting it to a string it's not the best solution, but for now it works
@@ -71,15 +73,50 @@ def get_project(projectid):
 
 @app.route("/getPeople", methods=['GET'])
 def get_people():
-    cursor = db.cursor(dictionary=True)
+    cursor = mysql.connection.cursor()
 
     cursor.execute("select * from people")
     people = cursor.fetchall()
+    cursor.close()
 
     return json.dumps(people)
 
 
+@app.route("/getPeopleFromProject/<projectid>", methods=['GET'])
+def get_people_from_project(projectid):
+    cursor = mysql.connection.cursor()
+
+    query = """
+        select personid, name, surname, role
+        from people natural join partecipates
+        where projectid = %s
+    """
+    tuple = (projectid,)
+
+    cursor.execute(query, tuple)
+    people = cursor.fetchall()
+    cursor.close()
+
+    return json.dumps(people)
+
 # ------------------- AREAS ------------------- #
+
+@app.route("/getAreasFromProject/<projectid>", methods=['GET'])
+def get_areas_from_project(projectid):
+    cursor = mysql.connection.cursor()
+
+    query = """
+        select areaid, title
+        from areas natural join refers
+        where projectid = %s
+    """
+    tuple = (projectid,)
+
+    cursor.execute(query, tuple)
+    areas = cursor.fetchall()
+    cursor.close()
+
+    return json.dumps(areas)
 
 
 if __name__ == '__main__':
