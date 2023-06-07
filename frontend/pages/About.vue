c9c3b0af86cb4e4803fc36a9e7760c7503c7f660
Francesco <dttfrancesco@gmail.com>
2023-06-08 00:24:56 +0200

    Modifiche about

--- frontend/pages/About.vue
+++ frontend/pages/About.vue
@@ -8,14 +8,20 @@ import PeopleCard from '@/components/PeopleCard.vue'
     <main>
         <div class="container-fluid">
             <br>
+            <br>
             <div class = "row my-4"></div>
-            <div class="row custom-heading mx-3">
-                <strong>About us</strong>
+            <div class="row custom-heading">
+                <div class ="col-md-1"></div>
+                <div class = "col-md-5">
+                    <strong class = "mx-3">About us</strong>
+                </div>
+                <div class = "col-md-6"></div>
             </div>
             <br>
             <br>
             <div class="row mx-3 justify-content-center">
-                <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class = "col-md-1"></div>
+                <div class="col-md-5 d-flex align-items-center justify-content-center">
                     <p class = "custom-h3" style="text-align:center;"> <b>“Embracing novel ideas, fueling unprecedented innovations, and
                             committing to the creation of a future where sustainability is not just a concept, but an
                             established practice.”</b></p>
@@ -25,8 +31,9 @@ import PeopleCard from '@/components/PeopleCard.vue'
             <br>
             <br>
             <br>
-            <div class="row mx-3">
-                <div class="col-md-6 my-3">
+            <div class="row mx-3 justify-content-center">
+                <div class="col-md-1"></div>
+                <div class="col-md-5 my-3">
                     <div class="my-auto">
                         We are a <strong>leading venture capital firm</strong> with a clear focus: to <strong>support and
                             foster groundbreaking ideas</strong> that have the potential to <strong>disrupt the
@@ -50,8 +57,8 @@ import PeopleCard from '@/components/PeopleCard.vue'
                             sustainable future through innovation and growth</strong>.
                     </div>
                 </div>
-                <div class="col-md-6 my-3 d-flex justify-content-center">
-                    <img class="card-img-top rounded d-flex justify-content-center" style="width: 70%; height: auto;" src="/images/about/usCats.webp" alt="Our Team">
+                <div class="col-md-5 my-3 d-flex justify-content-center">
+                    <img class="card-img-top rounded d-flex justify-content-center mr-2" style="width: 70%; height: auto;" src="/images/about/usCats.webp" alt="Our Team">
                 </div>
             </div>
             <div class="row my-5"></div>
@@ -66,20 +73,23 @@ import PeopleCard from '@/components/PeopleCard.vue'
                 </div>
                 <div class="col-md-2"></div>
                 <div class="col-md-5 rounded mb-3 text-center">
+                    <div class = "col-md-1"></div>
+                    <div class = "col-md-3"></div>
                     <div class="row" style="font-size: 3.5em;">
                         <p class = "custom-bold">$500M+</p>
                     </div>
                     <div class="row">
                         <p class = "custom-bold">Total Capital Deployed</p>
                     </div>
+                    <div class = "col-md-1"></div>
                 </div>
             </div>
             <div class="row my-5"></div>
             <div class="row mx-3">
                 <div class="col-md-6 my-3 d-flex justify-content-center align-items-center">
-                    <img class="card-img-top rounded" style="width: 70%; height: auto;" src="/images/about/invest.webp" alt="Investment Approach">
+                    <img class="card-img-top rounded ml-2" style="width: 70%; height: auto;" src="/images/about/invest.webp" alt="Investment Approach">
                 </div>
-                <div class="col-md-6 text-right my-3">
+                <div class="col-md-5 text-right my-3">
                     <div>
                         <h3> <b> Our Investment Approach </b></h3>
                     </div>
@@ -94,8 +104,9 @@ import PeopleCard from '@/components/PeopleCard.vue'
                         a broad range of industries, from technology to healthcare, energy to education. It's not just about
                         numbers for us; it's about the <strong>impact</strong>, the <strong>transformation</strong>. Our
                         partnership with these startups is a journey, a <strong>shared vision for a future</strong> where
-                        technology and innovation bring about widespread, positive change.</div>
+                        technology and innovation bring about a widespread change.</div>
                 </div>
+                <div class = "col-md-1"></div>
             </div>
             <div class="row my-4"></div>
             <div class="row mx-3 justify-content-center">
