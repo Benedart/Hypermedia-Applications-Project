736e8d14d4de901aa285c1b76c4ce3be05a8d77d
Benedart <49732385+Benedart@users.noreply.github.com>
2023-06-17 23:30:35 +0200

    Update nuxt.config.ts
    Thx reebo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
     runtimeConfig: {
         apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
         public: {
-            SERVER_URL: process.env.SERVER_URL || 'http://127.0.0.1:5000', // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+            SERVER_URL: 'http://reebo.it:5000', // can be overridden by NUXT_PUBLIC_API_BASE environment variable
         }
     },
 
